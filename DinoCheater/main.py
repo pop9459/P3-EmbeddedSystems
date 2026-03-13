a4b9f1bdaa3616c1e3289f625187e6da00999c3c
Peter Kapsiar <kapsiar888n@gmail.com>
2026-03-13 01:03:33 +0100

    started implementing duck mechanic in dino cheater

--- DinoCheater/main.py
+++ DinoCheater/main.py
@@ -9,12 +9,16 @@ from adafruit_hid.keycode import Keycode
 
 BUTTON_PIN = board.GP15
 BUILTIN_LED_PIN = board.LED
-LEFT_PHOTORESISTOR_PIN = board.GP27
-RIGHT_PHOTORESISTOR_PIN = board.GP28
+TOP_PHOTORESISTOR_PIN = board.GP27
+BOT_PHOTORESISTOR_PIN = board.GP28
 
-BLACK_TRESHHOLD = 3500
+TOP_BLACK_TRESHHOLD = 2000
+BOT_BLACK_TRESHHOLD = 3000
+
+SENSOR_HISTORY_SIZE = 10
 
 JUMP_KEY = Keycode.UP_ARROW
+DUCK_KEY = Keycode.DOWN_ARROW
 
 # Initialize the devices
 button_pin = digitalio.DigitalInOut(BUTTON_PIN)
@@ -23,8 +27,8 @@ button_pin.direction = digitalio.Direction.INPUT
 built_in_led = digitalio.DigitalInOut(BUILTIN_LED_PIN)
 built_in_led.direction = digitalio.Direction.OUTPUT
 
-left_photoresistor = analogio.AnalogIn(LEFT_PHOTORESISTOR_PIN)
-right_photoresistor = analogio.AnalogIn(RIGHT_PHOTORESISTOR_PIN)   
+top_photoresistor = analogio.AnalogIn(TOP_PHOTORESISTOR_PIN)
+bot_photoresistor = analogio.AnalogIn(BOT_PHOTORESISTOR_PIN)   
 
 # Initialize the keyboard
 kbd = Keyboard(usb_hid.devices)
@@ -32,26 +36,51 @@ kbd = Keyboard(usb_hid.devices)
 game_started = False
 built_in_led.value = False
 
+top_sensor_history = []
+bot_sensor_history = []
+
 while True:
+    top_value = top_photoresistor.value
+    bot_value = bot_photoresistor.value
+
+    top_sensor_history.append(top_value)
+    if len(top_sensor_history) > SENSOR_HISTORY_SIZE:
+        top_sensor_history.pop(0)
+
+    bot_sensor_history.append(bot_value)
+    if len(bot_sensor_history) > SENSOR_HISTORY_SIZE:
+        bot_sensor_history.pop(0)
+    
+    top_average = sum(top_sensor_history) / len(top_sensor_history)
+    bot_average = sum(bot_sensor_history) / len(bot_sensor_history)
+
+    print(f"Top AVG: {top_average}" f" Bot AVG: {bot_average}") 
+
     button_value = bool(button_pin.value)
     if(button_value):
         print("Starting game")
         game_started = not game_started
         built_in_led.value = game_started
-        time.sleep(1)
         if game_started:
             kbd.press(JUMP_KEY)
+            kbd.release(JUMP_KEY)
+        else:
+            kbd.release(JUMP_KEY)
+            kbd.release(DUCK_KEY)
+        time.sleep(1)
 
     if(game_started):
-        left_value = left_photoresistor.value
-        right_value = right_photoresistor.value
-
-        print("Left: {} Right: {}".format(left_value, right_value))
-
-        if(left_value < BLACK_TRESHHOLD):
-            time.sleep(0.2)
-            kbd.press(JUMP_KEY)
-            time.sleep(0.2)
-            kbd.release(JUMP_KEY)
+        # Avoid cactus
+        if(bot_average < BOT_BLACK_TRESHHOLD):
             print("Jump")
-            time.sleep(0.5)
+            kbd.press(JUMP_KEY)
+            time.sleep(0.1)
+            kbd.release(JUMP_KEY)
+        
+        # Avoid flying bird
+        if(top_average < TOP_PHOTORESISTOR_PIN and bot_average > BOT_BLACK_TRESHHOLD):
+            print("Duck")
+            time.sleep(1)
+            kbd.press(DUCK_KEY)
+            time.sleep(3)
+            kbd.release(DUCK_KEY)
